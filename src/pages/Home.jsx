980803de6bdef3a56f749fa842e54a43ed26e73a
nuwangikaushalya6-123 <nuwangikaushalya6@gmail.com>
2025-10-08 12:19:47 +0530

    Updated Home.jsx page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ const Home =()=>{
     return(
         <>
         <div className="text-{100px}">Welcome Home page
+
+            </div>
+
+             <div className="text-{100px}">Hotel
+            
             </div>
         </>
     )
